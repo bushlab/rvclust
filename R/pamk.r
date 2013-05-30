191b7d301f8fe52721aca3743da1e4c4975c2f11
Mike Sivley <mike.sivley@vanderbilt.edu>
2013-05-30 11:27:24 -0500

    Improved naming consistency and updated comments in pamk.r.

--- R/pamk.r
+++ R/pamk.r
@@ -1,43 +1,32 @@
 #' @include clustering.r
 NULL
 
-#' K-Medoids Clustering for rvclust (wrapper for fpc::pamk)
+#' K-Medoids Clustering for RVCLUST (wrapper for fpc::pamk)
 #'
-#' Performs K-Medoids clustering by wrapping the fpc::pamk function.\cr
-#' \cr
-#' Cluster assignment will be appended to the data$ped object.\cr
-#' The clusters object will be populated with SNP lists for each\cr
-#' cluster. The clusterinfo object will be populated with the\cr
-#' following information about each cluster:\cr
-#' 1. Chromosome\cr
-#' 2. Minimum Base Pair\cr
-#' 3. Maximum Base Pair\cr
-#' 4. Size\cr
-#' 5. Fitness\cr
-#' 6. Annotations (multi-column)\cr
+#' ---------------------------------------------------------------------- '#\cr
+#' This module wraps fpc::pamk to perform k-Medoids clustering. To apply \cr
+#' constraints, specify a set of columns that must be cluster homogenous. \cr
+#' ---------------------------------------------------------------------- '#\cr
 #'
 #' @author R Michael Sivley \email{mike.sivley@@vanderbilt.edu}
 #' @export
 #' @param rv rvclustobject
 #' @return clustered rvclustobject
-#' @seealso \code{\link{rvclustobject}}
-#' @seealso \code{\link{annotate}}
 pamk <- function(rv,label.by=NA,cluster.by=NA,constrain.by=NA) {
-  rarevariants <- rv$variants
-  raw.dat <- rv$observations
+  variants <- rv$variants
   
   if (is.na(label.by)) {label.by <- "SNP"}
   if (is.na(cluster.by)) {cluster.by <- "POS"}
 
-  # If a constraint variable is specified, force
+  # If a constraints are specified, force
   # homogenous clusters wrt constrain.by
   if (!is.na(constrain.by)) {
-    f <- rarevariants[,constrain.by]
-    rarevariants$CLUSTERID <- rep(0,length(f))
-    df.matrix <- split(rarevariants,f)
+    f <- variants[,constrain.by]
+    variants$CLUSTERID <- rep(0,length(f))
+    df.matrix <- split(variants,f)
   }
   else {
-    df.matrix <- list(rarevariants)
+    df.matrix <- list(variants)
   }
   
   # Cluster for each constrained data frame
@@ -51,29 +40,29 @@ pamk <- function(rv,label.by=NA,cluster.by=NA,constrain.by=NA) {
     df.matrix[[i]] <- df
   }
   
-  # Rebuild the rarevariants dataframe (unsplit)
-  rarevariants <- df.matrix[[1]]
+  # Rebuild the variants dataframe (unsplit) (1-op for unconstrained)
+  variants <- df.matrix[[1]]
   if (length(df.matrix) > 1) {
     for (i in 2:length(df.matrix)) {
-      rarevariants <- rbind(rarevariants,df.matrix[[i]])}
+      variants <- rbind(variants,df.matrix[[i]])}
   }
   
   # Create a vector of clustered SNPs
-  snp.clusters <- list()
-  length(snp.clusters) <- k
-  snp.clusters <- sapply(snp.clusters,function(x){rep('',0)})
-  for (i in 1:nrow(rarevariants)) {
-    cluster.id <- rarevariants[i,"CLUSTERID"]
-    snp <- as.character(rarevariants[i,label.by])
-    res <- append(snp.clusters[[cluster.id]],snp)
-    snp.clusters[[cluster.id]] <- res
+  clusters <- list()
+  length(clusters) <- k
+  clusters <- sapply(clusters,function(x){rep('',0)})
+  for (i in 1:nrow(variants)) {
+    cluster.id <- variants[i,"CLUSTERID"]
+    snp <- as.character(variants[i,label.by])
+    res <- append(clusters[[cluster.id]],snp)
+    clusters[[cluster.id]] <- res
   }
 
   # Create a cluster info dataframe
-  cluster.info <- create.cluster.dat(snp.clusters,rarevariants)
+  clusterinfo <- create.cluster.dat(clusters,variants)
 
-  rv$clusters <- snp.clusters
-  rv$clusterinfo <- cluster.info
-  rv$variants <- rarevariants
+  rv$clusters    <- clusters
+  rv$clusterinfo <- clusterinfo
+  rv$variants    <- variants
   return(rv)
 }
