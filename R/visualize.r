d72614554af50943544afc6e2e3fc51d1b1afd5c
Mike Sivley <mike.sivley@vanderbilt.edu>
2013-06-25 13:19:06 -0500

    Updated visualize.r to allow custom figure width and a limit on the number of variants to include for any one cluster. Also, duplicate p-values are now removed from the plot.

--- R/visualize.r
+++ R/visualize.r
@@ -5,16 +5,18 @@
 #' @author R Michael Sivley \email{mike.sivley@@vanderbilt.edu}
 #' @export
 #' @param rv rvclustobject
-visualize <- function(rvobj,label.by,phen.label='',top=10) {
+visualize <- function(rvobj,label.by,phen.label='',top=10,max.per.clus=20,fig.width=10) {
 
   # Prepare the cluster p-values
   temp.df <- rvobj$clusterinfo[,c('CLUSTERID','PVALUE')]
   temp.df <- temp.df[order(temp.df$PVALUE),]
-  print('cluster p-values (ordered')
+  print('cluster p-values (ordered)')
   print(temp.df$PVALUE)
   clus.sig.thresh <- 0.05 / nrow(temp.df)
   max <- min(top,nrow(temp.df))
   temp.df <- temp.df[1:max,]
+  print('cluster p-values (ordered & filtered)')
+  print(temp.df$PVALUE)
   clus.labels <- sapply(temp.df$CLUSTERID,function(x){paste("Cluster #",x,sep='')})
   temp.df[,label.by] <- clus.labels
   temp.df$COLOR="orange"
@@ -25,6 +27,20 @@ visualize <- function(rvobj,label.by,phen.label='',top=10) {
   df <- rvobj$variants[,c('CLUSTERID','PVALUE',label.by)]
   var.sig.thresh <- 0.05 / nrow(df)
   df <- df[df$CLUSTERID %in% temp.df$CLUSTERID,]
+  # Reduce to unique p-values and limit to max.per.clus
+  df.split <- split(df,df$CLUSTERID)
+  for (i in 1:length(df.split)) {
+    df <- df.split[[i]]
+    df <- df[!duplicated(df$PVALUE),]
+    max <- min(nrow(df),max.per.clus)
+    df <- df[order(df$PVALUE),]
+    df.split[[i]] <- df[1:max,]
+  }
+  # Reconstruct the data frame
+  df <- df.split[[1]]
+  for (i in 2:length(df.split)) {
+      df <- rbind(df,df.split[[i]])
+  }
   df$COLOR <- "blue"
   df <- rbind(df,temp.df)
 
@@ -39,8 +55,11 @@ visualize <- function(rvobj,label.by,phen.label='',top=10) {
   # Order the p-values
   df <- df[order(df$RANK,df$PVALUE),]
 
+  print('all (represented) p-values')
+  print(df$PVALUE)
+
   # Plot the p-values
-  pdf(paste('barplot_',phen.label,'.pdf',sep=''),height=9,width=10)
+  pdf(paste('barplot_',phen.label,'.pdf',sep=''),height=9,width=fig.width)
   #bplot <- barplot(-log(df$PVALUE),horiz=TRUE,col=df$COLOR,names.arg=df[,label.by],main="The Effect of Clustering on Rare Variant Significance",xlab="Inv Log P-Value",ylab=label.by)
   bplot <- barplot(-log(df$PVALUE),col=df$COLOR,las=2,cex.names=0.5,names.arg=df[,label.by],main="The Effect of Clustering on Rare Variant Significance",xlab=label.by,ylab="Inv Log P-Value")
   lines(x=bplot,y=rep(-log(var.sig.thresh),nrow(df)),col="blue")
